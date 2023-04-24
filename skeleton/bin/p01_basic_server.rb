23890c029570e48544ce8461ece467fbeb13e9a1
jliangrpb <jeremy.liang612@gmail.com>
2023-04-24 16:56:42 -0400

    Phase1

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,13 @@
 require 'rack'
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new 
+    res.write(req.path)
+    res['Content-Type'] = 'text/html'
+end
+
+Rack::Server.start({
+    app: app, 
+    Port: 3000
+})
